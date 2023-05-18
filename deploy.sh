3129c7bb9e2b161d8a3bc8d42200930c466897da
10inoino <inomatahi@icloud.com>
2023-05-18 13:50:22 +0000

    コマンド修正

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,7 @@
-gcloud run deploy go-template --source . --update-env-vars "DSN=$(printenv DSN)" --region asia-northeast1
+gcloud run deploy go-template --source . \
+    --set-env-vars "PSQL_DBNAME=$(printenv PSQL_DBNAME)" \
+    --set-env-vars "PSQL_HOST=$(printenv PSQL_HOST)" \
+    --set-env-vars "PSQL_USER=$(printenv PSQL_USER)" \
+    --set-env-vars "PSQL_PASS=$(printenv PSQL_PASS)" \
+    --set-env-vars "PSQL_SSLMODE=$(printenv PSQL_SSLMODE)" \
+    --region asia-northeast1
